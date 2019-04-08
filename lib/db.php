576e6209d29e0f7ef78e70b27dd98cf64499406c
xt <32516078+xtzero@users.noreply.github.com>
2019-04-09 01:55:37 +0800

    add trans

--- lib/db.php
+++ lib/db.php
@@ -45,6 +45,72 @@ class db{
             die();
         }
     }
+
+        $this->startTrans();
+        $resArr = [];
+        foreach($sqlArr as $k => $v){
+            $tempRes = mysqli_query($this->db,$v);
+            if(!$tempRes){
+                $this->rollback();
+                if(isset($failed) && $failed){
+                    return $failed([
+                        'sql' => $v,
+                        'res' => $tempRes,
+                        'mysqlError' => mysqli_error($this->db)
+                    ]);
+                }
+                
+                return false;
+            }
+        }
+
+        $this->commit();
+        if(isset($success) && $success){
+            return $success();
+        }
+        
+        return true;
+    }
+
+    public function startTrans(){
+        if($this->db){
+            $res = mysqli_query($this->db,'START TRANSACTION');
+            return $res;
+        }else{
+            error('db instance not found!');
+            die();
+        }
+    }
+
+    public function commit(){
+        if($this->db){
+            $res = mysqli_query($this->db,'COMMIT');
+            return $res;
+        }else{
+            error('db instance not found!');
+            die();
+        }
+    }
+
+    public function rollback(){
+        if($this->db){
+            $res = mysqli_query($this->db,'ROLLBACK');
+            return $res;
+        }else{
+            error('db instance not found!');
+            die();
+        }
+    }
+
+    public function error(){
+        if($this->db){
+            $res = mysqli_error($this->db);
+            return $res;
+        }else{
+            error('db instance not found!');
+            die();
+        }
+    }
 }
 
 function fetchDbResult($dbResult){
